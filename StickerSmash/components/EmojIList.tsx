a590f2f362c7f85c28ece07c50582f86caa517a2
RaissaBorgess <raissasantos123.rs33@gmail.com>
2025-04-03 11:28:17 -0300

    Criado o componente EmojiList

--- StickerSmash/components/EmojIList.tsx
+++ StickerSmash/components/EmojIList.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { StyleSheet, FlatList, Platform, Pressable} from 'react-native'
+import { StyleSheet, FlatList, Platform, Pressable } from 'react-native'
 import { Image, type ImageSource } from 'expo-image'
 
 type Props = {
@@ -7,12 +7,45 @@ type Props = {
     onCloseModal: () => void;
 }
 
-export default function EmojIList() {
+export default function EmojIList({ onSelect, onCloseModal}: Props) {
+  const [emoji] = useState<ImageSource[]>([
+  require('../assets/images.emoji1.png'),
+  require('../assets/images.emoji2.png'),
+  require('../assets/images.emoji3.png'),
+  require('../assets/images.emoji4.png'),
+  require('../assets/images.emoji5.png'),
+  require('../assets/images.emoji6.png'),
+])
   return (
-    <View>
-      <Text>EmojIList</Text>
-    </View>
+    <FlatList
+    horizontal
+    showsHorizontalScrollIndicator={Platform.OS === 'web'}
+    data={emoji}
+    contentContainerStyle = {styles.listContainer}
+    renderItem={({ item, index }) => (
+      <Pressable onPress={() => {
+        onSelect(item);
+        onCloseModal();
+      }}>
+        <Image source={item} key={index} style={styles.image} />
+      </Pressable>
+    )}
+    />
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20
+  }
+})
